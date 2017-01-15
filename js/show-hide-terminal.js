bbbe62bcd9e53a1bebb5c28052f2c29c679960b4
doliG <guillaume.lodi@gmail.com>
2017-01-15 21:38:26 +0100

    UX améliorée : - Hide/Show terminal : animation raccourcie lors du deuxieme affichage du terminal - Ajout dans le terminal d'une action : un appui sur la touche entrée quitte le terminal - Amélioration du code JS (structure sous forme de fonctions)

--- js/show-hide-terminal.js
+++ js/show-hide-terminal.js
@@ -5,20 +5,41 @@ $(function() {
                 e.preventDefault();
                 // console.log('click');
                 if ($('#terminal').hasClass('hidden')) {
-                    $('#terminal').removeClass('hidden');
-                    $('#terminal').addClass('animated bounceInUp')
-                    $('#content').addClass('hidden');
-                    $('#command').is(':focus');
-                    console.log('Terminal: show');
+                    terminalShow();
                 } else {
-                    $('#content').removeClass('hidden');
-                    $('#content').addClass('animated fadeIn')
-                    $('#terminal').addClass('hidden');
-                    console.log('Terminal: hide');
+                    terminalHide();
                 }
             }
-        } else if ( !$('#command').is(':focus') ) {
+        } else if (!$('#command').is(':focus')) {
             console.log("You should press space.");
         }
     })
-})
+
+    $('#command').keyup(function(e) {
+        if (e.keyCode == 27) {
+            e.preventDefault();
+            terminalHide();
+        }
+    });
+
+    function terminalShow() {
+        if ($("#terminal").hasClass("animated bounceInUp")) {
+            $("#terminal").removeClass("bounceInUp");
+            $("#terminal").addClass("fadeIn");
+        } else {
+            $("#terminal").addClass("animated bounceInUp");
+        }
+        $('#terminal').removeClass('hidden');
+        $('#content').addClass('hidden');
+        $("#terminal").bind('oanimationend animationend webkitAnimationEnd', function() {
+            $("#command").focus();
+        });
+        console.log('Terminal: show');
+    }
+    function terminalHide() {
+        $('#content').removeClass('hidden');
+        $('#content').addClass('animated fadeIn')
+        $('#terminal').addClass('hidden');
+        console.log('Terminal: hide');
+    }
+});
